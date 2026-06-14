faf393f1d7143da415a72583c92f38a8bca3b6b2
devzwide <nxumalobukeka66@gmail.com>
2026-06-14 06:32:08 +0200

    Add initial CSS and router setup
    - Created main.css to include Tailwind CSS.
    - Added router.tsx for basic routing with a home route.

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { RouterProvider } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import router from "./router.tsx";
+import "./main.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
+);
